639f626f3e29db47a223d0cd53d457fa2be42bf0
shraddhajawale <52071302+shraddhajawale@users.noreply.github.com>
2020-09-01 20:16:57 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page for UNNATI WEBHOOK v2.1</h1>
+  <marquee> Learning WEBHOOKS</marquee>
+<?php
+echo "Hello World! from GIT";
+echo "Hello from version1.1  ";
+echo " Why to Worry ? S2I in openshift";
+?>
+
+</body>
+</html>
